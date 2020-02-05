e00629b9771c186de7f570037173e2f950a72c0e
kevinpenny3 <kevin.penny3@yahoo.com>
2020-02-05 12:33:50 -0600

    base file structure

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+import 'typeface-roboto';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBpPKG8dNqBCowzdmRhiFSxFYIrSHtAWvg",
+    authDomain: "fitted-capstone.firebaseapp.com",
+    databaseURL: "https://fitted-capstone.firebaseio.com",
+    projectId: "fitted-capstone",
+    storageBucket: "fitted-capstone.appspot.com",
+    messagingSenderId: "298801174548",
+    appId: "1:298801174548:web:443990a5edc89edf5aef01"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
