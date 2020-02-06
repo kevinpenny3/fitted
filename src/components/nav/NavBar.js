5f81caaf5390739e0607d4d9384e169b6d88e554
kevinpenny3 <kevin.penny3@yahoo.com>
2020-02-06 09:33:37 -0600

    clothing properly adds to the database

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import "./Nav.css"
+import "./nav.css"
 import logo from "./fittedNavBar.png"
 
 export default (props) => {
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import * as firebase from 'firebase';
+import * as firebase from 'firebase';
 import { BrowserRouter as Router } from "react-router-dom";
 import Fitted from './components/Fitted';
 
 
-// var firebaseConfig = {
-//     apiKey: "AIzaSyBpPKG8dNqBCowzdmRhiFSxFYIrSHtAWvg",
-//     authDomain: "fitted-capstone.firebaseapp.com",
-//     databaseURL: "https://fitted-capstone.firebaseio.com",
-//     projectId: "fitted-capstone",
-//     storageBucket: "fitted-capstone.appspot.com",
-//     messagingSenderId: "298801174548",
-//     appId: "1:298801174548:web:443990a5edc89edf5aef01"
-//   };
-//   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
+var firebaseConfig = {
+    apiKey: "AIzaSyBpPKG8dNqBCowzdmRhiFSxFYIrSHtAWvg",
+    authDomain: "fitted-capstone.firebaseapp.com",
+    databaseURL: "https://fitted-capstone.firebaseio.com",
+    projectId: "fitted-capstone",
+    storageBucket: "fitted-capstone.appspot.com",
+    messagingSenderId: "298801174548",
+    appId: "1:298801174548:web:443990a5edc89edf5aef01"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
     <Router>
